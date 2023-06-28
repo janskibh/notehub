1b8bbddf2c1d7dabfe76d47ae4e2fc02f9ee9252
Jan Bellon <janski.bellon@gmail.com>
2023-06-28 16:06:21 +0200

    j

--- html/logout.php
+++ html/logout.php
@@ -2,11 +2,9 @@
 session_start();
 include '../include/config.php';
 include '../include/functions.php';
-if ($_SESSION['username'] != "22200239") {
-	$now = getdate();
-	$log = "D => " . sprintf("%02d", $now['mday']) . "/" . sprintf("%02d", $now['mon']) . "/" . $now['year'] . " " .sprintf("%02d", $now['hours']) . ":" . sprintf("%02d", $now['minutes']) . ":" . sprintf("%02d", $now['seconds']) . " -> " . $_SESSION['username'] . " s'est déconnecté depuis " . $_SERVER['REMOTE_ADDR'] . "\n"; 
-	addlog($log, $log_dir);
-}
+$now = getdate();
+$log = "D => " . sprintf("%02d", $now['mday']) . "/" . sprintf("%02d", $now['mon']) . "/" . $now['year'] . " " .sprintf("%02d", $now['hours']) . ":" . sprintf("%02d", $now['minutes']) . ":" . sprintf("%02d", $now['seconds']) . " -> " . $_SESSION['username'] . " s'est déconnecté depuis " . $_SERVER['REMOTE_ADDR'] . "\n"; 
+addlog($log, $log_dir);
 session_destroy();
 header('Location: login.php');
 exit();
